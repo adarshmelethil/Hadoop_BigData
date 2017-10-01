0cf27e96808fe88f41b00b0940aebdebad235f77
Adarsh Melethil <adarshmelethil@gmail.com>
2017-10-01 16:50:51 -0600

    10% regardless of blank lines

--- Assignment1/sub-sample/mapper.py
+++ Assignment1/sub-sample/mapper.py
@@ -13,10 +13,10 @@ def main(_):
 	for line in sys.stdin:
 		line = line.strip()
 		line = line.lower()
-		if len(line) > 0:
-			if random.random() < precent_keep:
+		
+		if random.random() < precent_keep:
+			if len(line) > 0:
 				print('{key}'.format(key=line))
-
 
 if __name__=='__main__':
 	parser = argparse.ArgumentParser(description='Mapper to reduce dataset by %')
