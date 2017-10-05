4aa355ebfadb840663de171fdacae60d104b8a2b
Adarsh Melethil <adarshmelethil@gmail.com>
2017-10-05 14:22:19 -0600

    fixed script

--- Assignment1/sorting/get_hdfs_content.sh
+++ Assignment1/sorting/get_hdfs_content.sh
@@ -15,7 +15,7 @@ echo "" > $FILE
 for (( num=0; num<28; num++ ))
 do
 	foo=$(printf "%05d" $num)
-	FIRST_ENTRY=hadoop fs -cat $OUTPUTFILE/part-$foo result.txt | head -n1
-	LAST_ENTRY=hadoop fs -cat $OUTPUTFILE/part-$foo result.txt | tail -n1
+	FIRST_ENTRY=$(hadoop fs -cat $OUTPUTFILE/part-$foo result.txt | head -n1)
+	LAST_ENTRY=$(hadoop fs -cat $OUTPUTFILE/part-$foo result.txt | tail -n1)
 	echo "$OUTPUTFILE/part-$foo: $FIRST_ENTRY - $LAST_ENTRY" >> $FILE
 done
