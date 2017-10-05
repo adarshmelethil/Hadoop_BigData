1a735ff1641a1a6e16079193ceb622f5a5af1664
Adarsh Melethil <adarshmelethil@gmail.com>
2017-10-05 14:21:20 -0600

    sript to see the first and last entry of the parts in the hadoop output

---
+++ Assignment1/sorting/get_hdfs_content.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "Checking head/tail of hadoop outputfile..."
+
+OUTPUTFILE=/user/adarsh.melethil/Assignment1/sorting/output
+FILE=hdfs_PartsInfo.txt
+
+while getopts "o" opt; do
+	case $opt in
+		o) OUTPUTFILE=$OPTARG;;
+	esac
+done
+
+echo "" > $FILE
+
+for (( num=0; num<28; num++ ))
+do
+	foo=$(printf "%05d" $num)
+	FIRST_ENTRY=hadoop fs -cat $OUTPUTFILE/part-$foo result.txt | head -n1
+	LAST_ENTRY=hadoop fs -cat $OUTPUTFILE/part-$foo result.txt | tail -n1
+	echo "$OUTPUTFILE/part-$foo: $FIRST_ENTRY - $LAST_ENTRY" >> $FILE
+done
