866969c1e76c0be19fdba2f272cdf837f264d31a
Adarsh Melethil <adarshmelethil@gmail.com>
2017-10-05 14:36:48 -0600

    removed error, had result.txt after cat command file path

--- Assignment1/sorting/get_hdfs_content.sh
+++ Assignment1/sorting/get_hdfs_content.sh
@@ -4,9 +4,10 @@ echo "Checking head/tail of hadoop outputfile..."
 OUTPUTFILE=/user/adarsh.melethil/Assignment1/sorting/output
 FILE=hdfs_PartsInfo.txt
 
-while getopts "o" opt; do
+while getopts ":f:o:" opt; do
 	case $opt in
 		o) OUTPUTFILE=$OPTARG;;
+		f) FILE=$OPTARG;;
 	esac
 done
 
@@ -15,7 +16,7 @@ echo "" > $FILE
 for (( num=0; num<28; num++ ))
 do
 	foo=$(printf "%05d" $num)
-	FIRST_ENTRY=$(hadoop fs -cat $OUTPUTFILE/part-$foo result.txt | head -n1)
-	LAST_ENTRY=$(hadoop fs -cat $OUTPUTFILE/part-$foo result.txt | tail -n1)
+	FIRST_ENTRY=$(hadoop fs -cat $OUTPUTFILE/part-$foo | head -n1)
+	LAST_ENTRY=$(hadoop fs -cat $OUTPUTFILE/part-$foo | tail -n1)
 	echo "$OUTPUTFILE/part-$foo: $FIRST_ENTRY - $LAST_ENTRY" >> $FILE
 done
