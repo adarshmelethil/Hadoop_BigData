c821a7c24065d16b40f0d0518ce5d6416e186c04
Adarsh Melethil <adarshmelethil@gmail.com>
2017-10-11 14:18:55 -0600

    fixed value=value

--- Assignment1/sorting/mapper.py
+++ Assignment1/sorting/mapper.py
@@ -19,7 +19,7 @@ def main():
 			
 			key1 = 0 if word[0] < 'a' else word[0]
 			key2 = word
-			value = value
+			value = word
 			print('{key1},{key2}\t{value}'.format(key1=key1, key2=key2, value=value))
 	
 
