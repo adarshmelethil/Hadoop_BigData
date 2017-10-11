8c814b1b331e6bf4a79f94c3e19f4c3460fd4ed5
Adarsh Melethil <adarshmelethil@gmail.com>
2017-10-11 15:34:09 -0600

    fixed mapper tester

--- Assignment1/sorting/run_real_testMapper.sh
+++ Assignment1/sorting/run_real_testMapper.sh
@@ -29,35 +29,11 @@ echo "Input file: $INPUTFILE"
 echo "Output file: $OUTPUTFILE"
 
 hadoop jar $JARFILE \
- # -D stream.map.output.field.separator=. \
- # -D map.output.key.field.separator=. \
- # -D stream.num.map.output.key.fields=2 \
  -D mapred.reduce.tasks=0 \
- # -D mapred.text.key.partitioner.option=-k1,1 \
  -files $MAPPER,$REDUCER \
  -mapper $MAPPER \
  -combiner $COMBINER \
  -reducer $REDUCER \
  -input $INPUTFILE \
  -output $OUTPUTFILE 
- # -partitioner org.apache.hadoop.mapred.lib.KeyFieldBasedPartitioner 
-
-#  
-
-
-# hadoop fs -getmerge $OUTPUTFILE result.txt
-# hadoop fs -rm -r $OUTPUTFILE
-
-# echo "Checking head/tail of hadoop outputfile..."
-
-
-# echo "" > $FILE
-
-# for (( num=0; num<27; num++ ))
-# do
-# 	foo=$(printf "%05d" $num)
-# 	FIRST_ENTRY=$(hadoop fs -cat $OUTPUTFILE/part-$foo | head -n1)
-# 	LAST_ENTRY=$(hadoop fs -cat $OUTPUTFILE/part-$foo | tail -n1)
-# 	echo "$OUTPUTFILE/part-$foo: $FIRST_ENTRY - $LAST_ENTRY" >> $FILE
-# done
-
+ 
