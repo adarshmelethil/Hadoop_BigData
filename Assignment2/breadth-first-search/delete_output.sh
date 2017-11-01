a76602a14c9fc8154c97d8de267162302e446e9d
Adarsh Melethil <adarshmelethil@gmail.com>
2017-11-01 16:44:21 -0600

    delete changed and loop fixed in run_real

--- Assignment2/breadth-first-search/delete_output.sh
+++ Assignment2/breadth-first-search/delete_output.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-OUTPUTFILE=/user/adarsh.melethil/Assignment2/breadth-first-search/output
-hadoop fs -rm -r $OUTPUTFILE
+OUTPUTFILE0=/user/adarsh.melethil/Assignment2/breadth-first-search/output0
+OUTPUTFILE1=/user/adarsh.melethil/Assignment2/breadth-first-search/output1
+
+hadoop fs -rm -r $OUTPUTFILE0
+hadoop fs -rm -r $OUTPUTFILE1
