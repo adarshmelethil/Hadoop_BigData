59e2252777ae6f6d3329ad780af15a9572546a60
madcowken <adarshmelethil@gmail.com>
2017-11-01 14:46:41 -0600

    commit before pull

--- Assignment2/breadth-first-search/run_real.sh
+++ Assignment2/breadth-first-search/run_real.sh
@@ -30,6 +30,6 @@ echo "Output file: $OUTPUTFILE"
 
 # hadoop jar $JARFILE -D stream.num.map.output.key.fields=2 -D mapred.text.key.partitioner.options=-k1,1 -D mapred.reduce.tasks=27 -mapper mapper.py -reducer reducer.py -partitioner org.apache.hadoop.mapred.lib.KeyFieldBasedPartitioner -file ./mapper.py -file ./reducer.py  -input /user/dkrishna/wordcount/shakespeare.txt -output $OUTPUTFILE
 
-hadoop jar $JARFILE -mapper $MAPPER -reducer $REDUCER -file ./$MAPPER -file ./$REDUCER  -input $INPUTFILE -output $OUTPUTFILE # > mapreduce_output.txt
+hadoop jar $JARFILE -mapper $MAPPER -reducer $REDUCER -file ./$MAPPER -file ./$REDUCER  -input $INPUTFILE -output $OUTPUTFILE | cat > mapreduce_output.txt
 
 
