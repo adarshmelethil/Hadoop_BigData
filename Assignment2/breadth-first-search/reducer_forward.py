90b1f561855edd219c60c138dd99ea17ced49695
Adarsh Melethil <adarshmelethil@gmail.com>
2017-11-04 10:42:35 -0600

    reducer_forward, to see what the reducer recives and what order

---
+++ Assignment2/breadth-first-search/reducer_forward.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#reducer.py
+
+import string
+import sys
+import random
+import argparse
+
+def main():
+	cur_node, cur_connections, cur_distance, cur_is_visted, cur_source = None, None, None, None, None 
+	for line in sys.stdin:
+		line = line.strip()
+		line = line.lower()
+		print(line);
+	
+
+if __name__ == "__main__":
+	main()
