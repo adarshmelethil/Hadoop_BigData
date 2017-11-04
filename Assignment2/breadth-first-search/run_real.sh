f9135e4e74c192eed9431f9d2807bf8d18045fd4
Adarsh Melethil <adarshmelethil@gmail.com>
2017-11-04 11:07:28 -0600

    using loop in script

--- Assignment2/breadth-first-search/run_real.sh
+++ Assignment2/breadth-first-search/run_real.sh
@@ -28,8 +28,7 @@ done
 echo "Maper: $MAPPER"
 echo "Reducer: $REDUCER"
 echo "Combiner: $COMBINER"
-echo "Input file: $INPUTFILE"
-echo "Output file: $OUTPUTFILE"
+
 
 # hadoop jar $JARFILE -D stream.num.map.output.key.fields=2 -D mapred.text.key.partitioner.options=-k1,1 -D mapred.reduce.tasks=27 -mapper mapper.py -reducer reducer.py -partitioner org.apache.hadoop.mapred.lib.KeyFieldBasedPartitioner -file ./mapper.py -file ./reducer.py  -input /user/dkrishna/wordcount/shakespeare.txt -output $OUTPUTFILE
 
@@ -38,35 +37,42 @@ OUTPUTFILE0=/user/adarsh.melethil/Assignment2/breadth-first-search/output0
 OUTPUTFILE1=/user/adarsh.melethil/Assignment2/breadth-first-search/output1
 OUTPUTFILE2=/user/adarsh.melethil/Assignment2/breadth-first-search/output2
 
-ALTERNATE=0
 
-NONBLACKCOUNT=1
 
-INPUTFILE=$OUTPUTFILE1/part-00000
-OUTPUTFILE=$OUTPUTFILE2
-hadoop jar $JARFILE -mapper $MAPPER -reducer $REDUCER -file ./$MAPPER -file ./$REDUCER  -input $INPUTFILE -output $OUTPUTFILE
+# INPUTFILE=$OUTPUTFILE1/part-00000
+# OUTPUTFILE=$OUTPUTFILE2
+# hadoop jar $JARFILE -mapper $MAPPER -reducer $REDUCER -file ./$MAPPER -file ./$REDUCER  -input $INPUTFILE -output $OUTPUTFILE
+
 # hadoop jar $JARFILE -mapper $MAPPER -reducer $REDUCER -file ./$MAPPER -file ./$REDUCER  -input $OUTPUTFILE0/part-00000 -output $OUTPUTFILE1
 # hadoop jar $JARFILE -mapper $MAPPER -reducer $REDUCER -file ./$MAPPER -file ./$REDUCER  -input $OUTPUTFILE1/part-00000 -output $OUTPUTFILE2
 
+ALTERNATE=0
+NONBLACKCOUNT=1
+while [ $NONBLACKCOUNT -gt 0 ];
+do 
+	if [ $ALTERNATE -eq 0 ]
+	then
+		# echo "USING OUTPUT0"
+		OUTPUTFILE=OUTPUTFILE0
+		echo "Input file: $INPUTFILE"
+		echo "Output file: $OUTPUTFILE"
+		hadoop jar $JARFILE -mapper $MAPPER -reducer $REDUCER -file ./$MAPPER -file ./$REDUCER  -input $INPUTFILE -output $OUTPUTFILE &> $HADOOPRESPONSE
+		ALTERNATE=1
+		INPUTFILE=$OUTPUTFILE0/part-00000
+		hadoop fs -rm -r $OUTPUTFILE1
+	else
+		# echo "USING OUTPUT1"
+		OUTPUTFILE=OUTPUTFILE1
+		echo "Input file: $INPUTFILE"
+		echo "Output file: $OUTPUTFILE"
+		hadoop jar $JARFILE -mapper $MAPPER -reducer $REDUCER -file ./$MAPPER -file ./$REDUCER  -input $INPUTFILE -output $OUTPUTFILE &> $HADOOPRESPONSE
+		ALTERNATE=0
+		INPUTFILE=$OUTPUTFILE1/part-00000
+		hadoop fs -rm -r $OUTPUTFILE0
+	fi
 
-# while [ $NONBLACKCOUNT -gt 0 ];
-# do 
-# 	if [ $ALTERNATE -eq 0 ]
-# 	then
-# 		echo "USING OUTPUT0"
-# 		hadoop jar $JARFILE -mapper $MAPPER -reducer $REDUCER -file ./$MAPPER -file ./$REDUCER  -input $INPUTFILE -output $OUTPUTFILE0 &> $HADOOPRESPONSE
-# 		ALTERNATE=1
-# 		INPUTFILE="$OUTPUTFILE0/part-00000"
-# 		hadoop fs -rm -r $OUTPUTFILE1
-# 	else
-# 		echo "USING OUTPUT1"
-# 		hadoop jar $JARFILE -mapper $MAPPER -reducer $REDUCER -file ./$MAPPER -file ./$REDUCER  -input $INPUTFILE -output $OUTPUTFILE1 &> $HADOOPRESPONSE
-# 		ALTERNATE=0
-# 		INPUTFILE="$OUTPUTFILE1/part-00000"
-# 		hadoop fs -rm -r $OUTPUTFILE0
-# 	fi
-# 	echo "GETING NONBLACKCOUNT"
-# 	NONBLACKCOUNT=$(cat $HADOOPRESPONSE | grep $COUNTNAME | cut -d '=' -f 2) 
-# 	echo "NONBLACKCOUNT: $NONBLACKCOUNT"
-# done 
+	echo "GETING NONBLACKCOUNT"
+	NONBLACKCOUNT=$(cat $HADOOPRESPONSE | grep $COUNTNAME | cut -d '=' -f 2) 
+	echo "NONBLACKCOUNT: $NONBLACKCOUNT"
+done 
 
